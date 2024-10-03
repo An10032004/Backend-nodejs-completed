a90c48b865a282fb1af06d032c576ac48815003e
duong an <duongansbsb@gmail.com>
2024-10-03 18:15:47 +0700

    client Page

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,24 @@
+const Product = require('../../models/product.model')
+const productsHelper = require('../../helpers/products')
+module.exports.index = async (req, res) => {
+    const productFeatured = await Product.find({
+        featured: "1",
+        deleted:false,
+        status:"active"
+    }).limit(6)
+    
 
-module.exports.index = async (req, res) => {     
+    const newProductsFeatured = productsHelper.priceNewProducts(productFeatured)
+
+    const productsNew = await Product.find({
+        deleted:false,
+        status:"active"
+    }).sort({ position: "desc" }).limit(6)
+    
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
     res.render("client/pages/homes/index", {
         pageTitle: "Trang chủ",
-
+        productFeatured:newProductsFeatured,
+        productsNew:newProductsNew
     }) 
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model")
-
+const ProductCategory = require("../../models/product-category.model")
+const productsCategoryHelper = require("../../helpers/product-category");
+const productsHelper = require("../../helpers/products");
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
@@ -25,11 +27,23 @@ module.exports.detail = async (req, res) => {
     try {
       const find = {
       deleted: false,
-      slug: req.params.slug,
+      slug: req.params.slugProduct,
       status:"active"
   }
 
   const product = await Product.findOne(find)
+
+  if(product.product_category_id){
+    const category = await ProductCategory.findOne({
+      _id:product.product_category_id,
+      status:"active",
+      deleted:false,
+    })
+
+    product.category = category
+  }
+
+  product.priceNew = productsHelper.priceNewProduct(product)
 
   res.render("client/pages/products/detail", {
       pageTitle: product.title,
@@ -38,6 +52,31 @@ module.exports.detail = async (req, res) => {
   } catch (error) {
       res.redirect(`/products`)       
   }
+}
+
+module.exports.category = async (req, res) => {
+    const category = await ProductCategory.findOne({
+      slug: req.params.slugCategory,
+      deleted:false,
+    })
+    const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+      product_category_id: { $in: [category.id, ...listSubCategoryId]},
+      deleted:false,
+    }).sort({position : "desc"})
+
+    const newProducts = products.map(item =>{
+      item.priceNew = (item.price*(100 - item.discountPercentage) / 100).toFixed(0)
+      return item
+    })
+    
+    res.render("client/pages/products/index", {
+      pageTitle: category.title,
+      products: newProducts
+    });
 }
    
     
